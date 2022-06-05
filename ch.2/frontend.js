3b89efbe5d8a24200ba63c5c3b93c36c12a4a3fa
Seungmin Nam <nhjbest22@g.skku.edu>
2022-06-05 11:18:55 +0000

    Backend Test using axios

--- ch.2/frontend.js
+++ ch.2/frontend.js
@@ -1,24 +1,27 @@
-(async () => {
-    try {
-    const result = await axios.get("https://www.zerocho.com/api/get");
-    console.log(result);
-    console.log(result.data);
-    } catch (error) {
-    console.error(error);
+(async ()=>{
+    try{
+        const encode = encodeURIComponent("노드");
+        const result = await axios.get("https://www.zerocho.com/api/search/" + encode);
+        console.log("https://www.zerocho.com/api/search/" + encode)
+        console.log(result);
+        console.log(result.data);
+    }
+    catch(error){
+        console.error(error)
     }
 })();
-(async () => {
-    try {
-    const result = await axios.post(
-        "https://www.zerocho.com/api/pos/json",
-        {
-        name: "zerocho",
-        birth: 1994
-        }
-    );
-    console.log(result);
-    console.log(result.data);
-    } catch (error) {
-    console.error(error);
-    }
-})();
+
+// (async()=>{
+//     try{
+//         const formData = new FormData();
+//         formData.append("name", "zerocho");;
+//         formData.append("birth", "1994");
+
+//         const result = await axios.post("https://www.zerocho.com/api/post", formData);
+//         console.log(result);
+//         console.log(result.data);    
+//     }
+//     catch(error){
+//         console.error(error);
+//     }
+// })
