70b76bdb71e4d6ca9705f1496dfb2aa5b3509b96
nhjbest22 <nhjbest22@g.skku.edu>
2022-06-11 21:42:04 +0900

    Restfull Server test (nice work :D)

--- ch.4/ch.4.2/restFront.js
+++ ch.4/ch.4.2/restFront.js
@@ -1,63 +1,67 @@
-async function getUser() { // 로딩 시 사용자 가져오는 함수
-    try {
-      const res = await axios.get('/users');
-      const users = res.data;
-      const list = document.getElementById('list');
-      list.innerHTML = '';
-      // 사용자마다 반복적으로 화면 표시 및 이벤트 연결
-      Object.keys(users).map(function (key) {
-        const userDiv = document.createElement('div');
-        const span = document.createElement('span');
-        span.textContent = users[key];
-        const edit = document.createElement('button');
-        edit.textContent = '수정';
-        edit.addEventListener('click', async () => { // 수정 버튼 클릭
-          const name = prompt('바꿀 이름을 입력하세요');
-          if (!name) {
-            return alert('이름을 반드시 입력하셔야 합니다');
-          }
-          try {
-            await axios.put('/user/' + key, { name });
-            getUser();
-          } catch (err) {
-            console.error(err);
-          }
+async function getUser(){
+    try{
+        console.log(`getUser 함수 실행`)
+        const res = await axios.get("/users")
+        const users = res.data;
+        const list = document.getElementById("list");
+        list.innerHTML = "";
+        // 사용자마다 반복적으로 화면 표시 및 이벤트 연결
+        Object.keys(users).map(function (key){ // Object.key(users) => users 객체에서 key값을 뽑아낸 배열 생성, map 함수로 key값 배열 속 각각의 key에 대해 함수 실행
+            const userDiv = document.createElement("div")
+            const span = document.createElement("span");
+            span.textContent = users[key];
+            const edit = document.createElement("button");
+            edit.textContent = "수정";
+            edit.addEventListener("click",async ()=>{
+                const name = prompt("바꿀 이름을 입력하세요.");
+                if(!name){
+                    return alert("이름을 반드시 입력하세요 합니다.");
+                }
+                try{
+                    await axios.put("/user/" + key, {name});
+                    getUser();
+                }
+                catch(err){
+                    console.error(err);
+                }
+            });
+            const remove = document.createElement("button");
+            remove.textContent = "삭제";
+            remove.addEventListener("click",async ()=>{
+                try{
+                    await axios.delete("/user/" + key);
+                    getUser();
+                }
+                catch(err){
+                    console.error(err);
+                }
+            });
+            userDiv.appendChild(span);//value값(내가 입력한 이름) 첨가
+            userDiv.appendChild(edit);//수정 버튼 추가
+            userDiv.appendChild(remove);//삭제 버튼 추가
+            list.appendChild(userDiv);//유저 객체를 html에 있는 list 에 추가
+            console.log(`res.data : ${res.data}`);
         });
-        const remove = document.createElement('button');
-        remove.textContent = '삭제';
-        remove.addEventListener('click', async () => { // 삭제 버튼 클릭
-          try {
-            await axios.delete('/user/' + key);
-            getUser();
-          } catch (err) {
-            console.error(err);
-          }
-        });
-        userDiv.appendChild(span);
-        userDiv.appendChild(edit);
-        userDiv.appendChild(remove);
-        list.appendChild(userDiv);
-        console.log(res.data);
-      });
-    } catch (err) {
-      console.error(err);
     }
-  }
-  
-  window.onload = getUser; // 화면 로딩 시 getUser 호출
-  // 폼 제출(submit) 시 실행
-  document.getElementById('form').addEventListener('submit', async (e) => {
+    catch(err){
+        console.error(err);
+    }
+}
+
+window.onload = getUser; // 화면 로딩 시 getuser 함수 실행
+document.getElementById("form").addEventListener('submit', async (e)=>{
     e.preventDefault();
     const name = e.target.username.value;
-    if (!name) {
-      return alert('이름을 입력하세요');
+    if(!name){
+        return alert("이름을 입력하세요");
     }
-    try {
-      await axios.post('/user', { name });
-      getUser();
-    } catch (err) {
-      console.error(err);
+    console.log(`form 에서 받아오는 name value : ${name}`)
+    try{
+        await axios.post("/user" , {name});
+        getUser();
     }
-    e.target.username.value = '';
-  });
-  
+    catch(err){
+        console.error(err);
+    }
+    e.target.username.value = "";
+})
--- ch.4/ch.4.2/restServer.js
+++ ch.4/ch.4.2/restServer.js
@@ -1,77 +1,86 @@
-const http = require('http');
-const fs = require('fs').promises;
+const http = require("http");
+const fs = require("fs").promises;
+const { stringify } = require("querystring");
 
-const users = {}; // 데이터 저장용
+const users = {}; //데이터 저장용
 
-http.createServer(async (req, res) => {
-  try {
-    if (req.method === 'GET') {
-      if (req.url === '/') {
-        const data = await fs.readFile('./restFront.html');
-        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-        return res.end(data);
-      } else if (req.url === '/about') {
-        const data = await fs.readFile('./about.html');
-        res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-        return res.end(data);
-      } else if (req.url === '/users') {
-        res.writeHead(200, { 'Content-Type': 'application/json; charset=utf-8' });
-        return res.end(JSON.stringify(users));
-      }
-      // /도 /about도 /users도 아니면
-      try {
-        const data = await fs.readFile(`.${req.url}`);
-        return res.end(data);
-      } catch (err) {
-        // 주소에 해당하는 라우트를 못 찾았다는 404 Not Found error 발생
-      }
-    } else if (req.method === 'POST') {
-      if (req.url === '/user') {
-        let body = '';
-        // 요청의 body를 stream 형식으로 받음
-        req.on('data', (data) => {
-          body += data;
-        });
-        // 요청의 body를 다 받은 후 실행됨
-        return req.on('end', () => {
-          console.log('POST 본문(Body):', body);
-          const { name } = JSON.parse(body);
-          const id = Date.now();
-          users[id] = name;
-          res.writeHead(201, { 'Content-Type': 'text/plain; charset=utf-8' });
-          res.end('ok');
-        });
-      }
-    } else if (req.method === 'PUT') {
-      if (req.url.startsWith('/user/')) {
-        const key = req.url.split('/')[2];
-        let body = '';
-        req.on('data', (data) => {
-          body += data;
-        });
-        return req.on('end', () => {
-          console.log('PUT 본문(Body):', body);
-          users[key] = JSON.parse(body).name;
-          res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-          return res.end('ok');
-        });
-      }
-    } else if (req.method === 'DELETE') {
-      if (req.url.startsWith('/user/')) {
-        const key = req.url.split('/')[2];
-        delete users[key];
-        res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-        return res.end('ok');
-      }
+http.createServer(async (req, res)=>{
+    try{
+        console.log(req.method, req.url);
+        if(req.method === "GET"){
+            if(req.url === "/"){
+                console.log("/로 진입")
+                const data = await fs.readFile("./restFront.html");
+                res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+                return res.end(data);
+            }else if(req.url === "/about"){
+                const data = await fs.readFile("./about.html");
+                res.writeHead(200,{"Content-Type": "text/html; charset=utf-8"});
+                return res.end(data);
+            }
+            else if(req.url === "/users"){
+                res.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+                console.log(`저장되어 있는 user들 : ${JSON.stringify(users)}`);
+                return res.end(JSON.stringify(users));
+            }
+            try{
+                const data = await fs.readFile(`.${req.url}`);
+                console.log(`url 요청 : ${req.url}`)
+                return res.end(data);
+            }
+            catch(err){
+                console.log("해당하는 url이 존재하지 않습니다.");
+                console.error(err);
+            }
+        }
+        else if(req.method === "POST"){
+            if(req.url==="/user"){
+                let body = '';
+                req.on('data', (data)=>{
+                    body += data;
+                });
+                console.log(`Body 본문 : ${body}`)
+                return req.on('end', ()=>{
+                    console.log("POST 본문(Body):", body); //body = {name : 입력값}
+                    const {name} = JSON.parse(body);
+                    const id = Date.now();
+                    users[id] = name; // users = {"~~~~ 무작위 숫자" : "내가 넣은 값"}
+                    res.writeHead(201);
+                    res.end("등록 성공");
+                })
+            }
+        }
+        else if(req.method === "PUT"){
+            if(req.url.startsWith('/user/')){
+                const key = req.url.split("/")[2]; // split으로 나눠지는 값 : "", "user", "찾는 값" <= 찾는 값이 split[2]
+                console.log(`나눠지는 key value : ${req.url.split("/")}`)
+                let body = "";
+                req.on('data', (data)=>{
+                    body += data;
+                });
+                return req.on('end', ()=>{
+                    console.log("PUT 본문(Body):", body);
+                    users[key] = JSON.parse(body).name;
+                    return res.end(JSON.stringify(users));
+                });
+            }
+        }
+        else if (req.method === "DELETE"){
+            if(req.url.startsWith("/user/")){
+                const key = req.url.split("/")[2];
+                delete users[key];
+                return res.end(JSON.stringify(users));
+            }
+        }
+        res.writeHead(404);
+        res.end("NOT FOUND");
     }
-    res.writeHead(404);
-    return res.end('NOT FOUND');
-  } catch (err) {
-    console.error(err);
-    res.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
-    res.end(err.message);
-  }
+    catch(err){
+        console.error(err);
+        res.writeHead(500);
+        res.end(err);
+    }
 })
-  .listen(8082, () => {
-    console.log('8082번 포트에서 서버 대기 중입니다');
-  });
+    .listen(port = 8082, ()=>{
+        console.log(`${port}번 포트에서 서버 대기 중입니다.`);
+    });
